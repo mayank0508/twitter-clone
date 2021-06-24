464f876eef7d8cbee8c44491450cc5556536bbdb
mayank0508 <thakurmayank0508@gmail.com>
2021-06-24 13:03:44 +0530

    Header Declared

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi bois !!</h1>
+     <Header />
     </div>
   );
 }
